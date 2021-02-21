a9d3ea4b7d75d1843fa8c5a0d930c6f9550cd5ec
Jorge I <jorgecedeno@csus.edu>
2021-02-21 15:52:00 -0800

    Add files via upload

---
+++ fancifymytext.js
@@ -0,0 +1,25 @@
+
+function myFunction(){
+    document.getElementById("button"); 
+    document.getElementById("text-box").style.fontSize = "24pt";
+}
+
+function fancy() {
+    document.getElementById("FancyShmancy");
+    document.getElementById("text-box").style.fontFamily = "fantasy";
+    document.getElementById("text-box").style.fontWeight = "bold";
+    document.getElementById("text-box").style.color = "blue";
+    document.getElementById("text-box").style.backgroundColor = "beige";
+    document.getElementById("text-box").style.textDecoration = "underline";
+
+}
+
+function boring() {
+    document.getElementById("BoringBetty");
+    document.getElementById("text-box").style.fontFamily = "inherit";
+    document.getElementById("text-box").style.fontWeight = "inherit";
+    document.getElementById("text-box").style.color = "inherit";
+    document.getElementById("text-box").style.backgroundColor = "inherit";
+    document.getElementById("text-box").style.textTransform = "inherit";
+}
+
